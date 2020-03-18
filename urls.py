c21d49ba3630926354ddc797ec384b56f9d351ff
hrithvika <53346205+hrithvika@users.noreply.github.com>
2020-03-18 10:58:30 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('^home/$', views.index, name='index'),
-    path('^ads/$', views.ad, name='ad'),
-    path('^bookings/<int:game_id>/$', views.booking, name='booking'),
-]
